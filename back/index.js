235113a4f55dd69f3a46375df0013f94cab0acad
beomhyunkim <beomhyunkim@gmail.com>
2019-07-23 15:45:02 +0900

    port번호 수정 프론트 테스트용 : 10123 , 백 테스트용 : 11123

--- back/index.js
+++ back/index.js
@@ -10,4 +10,4 @@ swaggerDoc(app);
 
 app.use((err,req,res,next) => console.error('There was an error',err));
 
-app.listen(10123,()=>console.log('App started'));
+app.listen(11123,()=>console.log('App started'));
