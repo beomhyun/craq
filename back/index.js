ab1130a631084e335605af7332f8d338b12b3bf4
beomhyunkim <beomhyunkim@gmail.com>
2019-07-25 13:31:08 +0900

    회원가입시 프로필 기본값 생성, 프로필 수정, 이미지 업로드,  이미지 출력

--- back/index.js
+++ back/index.js
@@ -4,11 +4,17 @@ const topic = require('./model/topic');
 const swaggerDoc = require('./swaggerDoc');
 const bodyParser = require('body-parser');
 const app = express(express);
-
+app.use(express.static('upload'));
 app.use(bodyParser.json());
 endpoints(app);
 topic(app);
 swaggerDoc(app);
+
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
 
 app.use((err,req,res,next) => console.error('There was an error',err));
 
--- back/model/user.js
+++ back/model/user.js
@@ -2,6 +2,114 @@ var mysql_dbc = require('../db/db_con.js')();
 var connection = mysql_dbc.init();
 const jwt = require("jsonwebtoken");
 const secretObj = require("../config/jwt");
+const multer = require('multer');
+//const storage = multer.memoryStorage();
+const path = require("path");
+
+let storage = multer.diskStorage({
+    destination: function(req, file ,callback){
+        callback(null, "upload/")
+    },
+    filename: function(req, file, callback){
+        callback(null, new Date().valueOf() + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage });
+
+/**
+ * @swagger
+ * definitions:
+ *   userInfo:
+ *     type: object
+ *     properties:
+ *       pk:
+ *         type: integer
+ *         description: user's id
+ *       Permission:
+ *         type: integer
+ *         description: user's permission (0 일반 , 1 관리자)
+ *       email:
+ *         type: string
+ *         description: user's email(unique)
+ *       username:
+ *         type: string
+ *         description: user's name(unique)
+ *       password:
+ *         type: string
+ *         description: user's password (암호화해서 저장)
+ *       last_login:
+ *         type: timestamp
+ *         description: user's last login time
+ *       createdUser:
+ *         type: integer
+ *         description: id of user who create the information
+ *       updatedUser:
+ *         type: integer
+ *         description: id of user who update the information
+ *       is_public:
+ *         type: integer
+ *         description: visibilty check
+ *       is_active:
+ *         type: integer
+ *         description: activity check
+ *       is_removed:
+ *         type: integer
+ *         description: remove check
+ *       ip_address:
+ *         type: string
+ *         description: user's ip
+ *       info:
+ *         type: string
+ *         description: information or detailed description
+ *   user pofile Info:
+ *     type: object
+ *     properties:
+ *       User:
+ *         type: integer
+ *         description: user's id
+ *       profile_image:
+ *         type: string
+ *         description: user's profile image (경로)
+ *       ssafy_years:
+ *         type: integer
+ *         description: user's ssafy year
+ *       is_major:
+ *         type: boolean
+ *         description: user's major check
+ *       region:
+ *         type: string
+ *         description: user's region
+ *       grade:
+ *         type: string
+ *         description: user's SW grade
+ *       intro:
+ *         type: string
+ *         description: user's intro
+ *       gitUrl:
+ *         type: string
+ *         description: user's gitUrl
+ *       createdUser:
+ *         type: integer
+ *         description: id of user who create the information
+ *       updatedUser:
+ *         type: integer
+ *         description: id of user who update the information
+ *       is_public:
+ *         type: integer
+ *         description: visibilty check
+ *       is_active:
+ *         type: integer
+ *         description: activity check
+ *       is_removed:
+ *         type: integer
+ *         description: remove check
+ *       ip_address:
+ *         type: string
+ *         description: user's ip
+ *       info:
+ *         type: string
+ *         description: information or detailed description
+ */
 
 const initializeEndpoints = (app)=>{
   /**
@@ -29,6 +137,75 @@ const initializeEndpoints = (app)=>{
             });
     }
 );
+/**
+ * @swagger
+ *  /email-checking/{email}:
+ *    get:
+ *      tags:
+ *      - users
+ *      description: 해당 이메일의 중복 체크
+ *      responses:
+ *       200:
+ *      parameters:
+ *       - in: path
+ *         name: email
+ *         type: string
+ *         description: |
+ *          사용자 이메일 전달
+ */
+app.get('/email-checking/:email', function(req,res){
+    var sql = " select count(*) as checking from user where email like ? ";
+    var params = [req.params.email];
+    connection.query(sql,params, function(err, rows, fields) {
+            if (!err){
+              if(rows[0].checking == 0){
+                res.send({status: "success"});
+              }else{
+                res.send({status: "fail"});
+              }
+            }
+            else{
+              console.log('Error while performing Query.', err);
+              res.send(err);
+            }
+          });
+  }
+);
+/**
+ * @swagger
+ *  /username-checking/{username}:
+ *    get:
+ *      tags:
+ *      - users
+ *      description: 해당 이름 중복 체크
+ *      responses:
+ *       200:
+ *      parameters:
+ *       - in: path
+ *         name: username
+ *         type: string
+ *         description: |
+ *          사용자 이름 전달
+ */
+app.get('/username-checking/:username', function(req,res){
+    var sql = " select count(*) as checking from user where username like ? ";
+    var params = [req.params.username];
+    connection.query(sql,params, function(err, rows, fields) {
+            if (!err){
+              if(rows[0].checking == 0){
+                res.send({status: "success"});
+              }else{
+                res.send({status: "fail"});
+              }
+            }
+            else{
+              console.log('Error while performing Query.', err);
+              res.send(err);
+            }
+          });
+  }
+);
+
 
 /**
  * @swagger
@@ -123,7 +300,7 @@ app.get('/users/email/:email', function(req,res){
   app.post('/users', function(req,res){
       console.log(req.body);
       if(req.body.email == null){
-        res.status(400).send({error:'email value need'});
+        res.status(200).send({status: "fail", error:' already used.'});
       }
       var usedcheck = " select count(*) as usedcheck from user where email like ? or username like ?";
       var checkparams = [req.body.email, req.body.username];
@@ -132,19 +309,24 @@ app.get('/users/email/:email', function(req,res){
                 res.send(err);
               }
               else if(rows[0].usedcheck != 0){
-                res.status(400).send({error:' already used.'});
+                res.status(200).send({status: "fail", error:' already used.'});
               }else{
                 var sql = " insert into user(email, username, password) values(?,?,?) ";
                 var params = [req.body.email,req.body.username,req.body.password,req.body.status,req.body.profile,req.body.git_address];
                 connection.query(sql,params, function(err, rows, fields) {
                         if (!err){
-                          res.json(rows);
+                            //user profile
+                          sql = " insert into profile(User) values((select u.pk from user as u where username like ?)) ";
+                          params = [req.body.username];
+                          connection.query(sql,params, function(err, rows, fields){});
+                          res.send({status: "success"});
                         }
                         else{
                           console.log('Error while performing Query.', err);
                           res.send(err);
                         }
                       });
+                
               }
             });
     }
@@ -172,12 +354,12 @@ app.get('/users/email/:email', function(req,res){
    */
   app.post('/login', function(req,res){
       console.log(req.body);
-      var sql = " select count(*) as c from user where email = ? and password = ? and is_removed = 0 ";
+      var sql = " select count(*) as checking, pk, username from user where email = ? and password = ? and is_removed = 0 ";
       var params = [req.body.email,req.body.password];
       connection.query(sql,params, function(err, rows, fields) {
               if (!err){
-                console.log(JSON.stringify(rows[0].c));
-                if(rows[0].c == 1){
+                console.log(JSON.stringify(rows[0].checking));
+                if(rows[0].checking == 1){
 
                   let token = jwt.sign({
                     email : req.body.email
@@ -188,8 +370,8 @@ app.get('/users/email/:email', function(req,res){
                   })
 
                   console.log("ok");
-                  res.cookie("email",token);
-                  res.send({status:"success", data: token});
+                  res.cookie("jwt",token);
+                  res.send({status:"success", jwt: token,pk: rows[0].pk, username: rows[0].username });
                 }else{
                   console.log("fail");
                   res.send({status: "fail"});
@@ -443,6 +625,147 @@ app.get('/follows', function(req,res){
   });
 }
 );
+
+/**
+ * @swagger
+ *  /profile:
+ *    put:
+ *      tags:
+ *      - users
+ *      description: 유저 프로필 등록, 수정
+ *      responses:
+ *       200:
+ *      parameters:
+ *       - in: query
+ *         name: User
+ *         type: integer
+ *       - in: query
+ *         name: ssafy_years
+ *         type: integer
+ *       - in: query
+ *         name: is_major
+ *         type: integer
+ *       - in: query
+ *         name: region
+ *         type: string
+ *       - in: query
+ *         name: grade
+ *         type: string
+ *       - in: query
+ *         name: intro
+ *         type: string
+ *       - in: query
+ *         name: gitUrl
+ *         type: string
+ *       - in: formData
+ *         name: profile_image
+ *         type: file
+ */
+app.put('/profile', upload.single('profile_image'), function(req, res){
+    console.log(req.query);
+    console.log(req.file);
+    var sql = "";
+    var params = [];
+    if(req.file){
+//      console.log("파일있음 !");
+      sql = " update profile set ssafy_years = ?, is_major = ?, region = ?, grade = ?, intro= ?, gitUrl = ?, profile_image = ?, updated_at = now() where User = ? ";
+      params = [req.query.ssafy_years, req.query.is_major, req.query.region, req.query.grade, req.query.intro, req.query.gitUrl, req.file.filename, req.query.User];      
+    }else{
+//      console.log("파일없음 !");
+      sql = " update profile set ssafy_years = ?, is_major = ?, region = ?, grade = ?, intro= ?, gitUrl = ?, updated_at = now() where User = ? ";
+      params = [req.query.ssafy_years, req.query.is_major, req.query.region, req.query.grade, req.query.intro, req.query.gitUrl, req.query.User];
+
+    }
+    connection.query(sql, params, function(err, rows, fields) {
+            if (err){
+              console.log(err);
+              res.send({status: "fail"});
+            }else{
+              res.send({status: "success"});
+            }
+          });
+  }
+);
+
+/**
+ * @swagger
+ *  /users/profile-image/{pk}:
+ *    get:
+ *      tags:
+ *      - users
+ *      description: 해당 회원 프로필 이미지를 가져온다.
+ *      responses:
+ *       200:
+ *      parameters:
+ *       - in: path
+ *         name: pk
+ *         type: integer
+ *         description: |
+ *          사용자 pk 전달
+ */
+app.get('/users/profile-image/:pk', function(req,res){
+      var sql = " select profile_image from profile where user = ? ";
+      var params = [req.params.pk];
+      connection.query(sql,params, function(err, rows, fields) {
+        if (!err){
+          var img = '<img src="/'+rows[0].profile_image + '">';
+          res.send(img);
+        }else{
+          res.send({status: "fail"});
+        }
+      });    
+}
+);
+/**
+ * @swagger
+ *  /users/profile/{pk}:
+ *    get:
+ *      tags:
+ *      - users
+ *      description: 해당 회원 프로필 정보를 가져온다.
+ *      responses:
+ *       200:
+ *      parameters:
+ *       - in: path
+ *         name: pk
+ *         type: integer
+ *         description: |
+ *          사용자 pk 전달
+ */
+app.get('/users/profile/:pk', function(req,res){
+  var sql = " select * from profile where user = ? ";
+  var params = [req.params.pk];
+  connection.query(sql,params, function(err, rows, fields) {
+    if (!err){
+      res.send(rows);
+    }else{
+      res.send({status: "fail"});
+    }
+  });    
+}
+);
+
+/**
+ * @swagger
+ *  /uploadtest:
+ *    post:
+ *      tags:
+ *      - users
+ *      description: 회원 가입. <br><br>  중복체크 <br> - This email is already in use.
+ *      responses:
+ *       200:
+ *      parameters:
+ *       - in: formData
+ *         name: upfile1
+ *         type: file
+ */
+app.post('/uploadtest', upload.single('upfile1'), function(req, res){
+  res.send('Uploaded! : '+req.file.originalname); // object를 리턴함
+  console.log(req.file.originalname); // 콘솔(터미널)을 통해서 req.file Object 내용 확인 가능.
+
+});
+
+
 
 };
 
