4de7e9e436c67e22ad2c1b890ec1360338374000
KGH <rngus3050@gmail.com>
2019-07-26 10:51:32 +0900

    Footer && Contact Us

--- craq/src/components/Footer.vue
+++ craq/src/components/Footer.vue
@@ -1,21 +1,25 @@
 <template>
-    <footer class="main-footer padding-y-lg">
+    <footer class="main-footer padding-y-lg" >
         <div class="container max-width-lg">
 
             <div class="main-footer__colophon">
 
-                <div class="main-footer__colophon-nav">
+                <div class="main-footer__colophon-nav" >
                     <span>&copy; myWebsite</span>
-                    <button @click="showModal=true">&nbsp; Contact Us</button>
+                    <span @click="openModal" class="contact">&nbsp; Contact Us</span>
                     
                     <mailModal v-bind:show="showModal" @close="closeModal"></mailModal>
                 </div>
 
                 <!-- DropDown Test -->
                 <div @click="toggle" class="dropdowns" :class="{'open' : config.open == true}">
-                  {{this.config.placeholder}}
-                  <ul class="dropdowns-menu" v-for="option in config.menus">
-                    <a :href="option.urls" target="_blank"><li @mouseover="config.placeholder = option.title">{{option.title}}</li></a>
+                  SSAFY.Site &nbsp;
+                  <ul class="dropdowns-menu" v-for="option in config.menus"  @mouseover="config.placeholder = option.title">
+                    <a :href="option.urls" target="_blank"> 
+                      <li>
+                        <img :src="option.imag" alt="ssafy" style="width:20px; display:inline;">&nbsp; {{option.title}}
+                        </li>
+                      </a>
                   </ul>
                 </div>
 
@@ -42,24 +46,28 @@ export default {
       config: {
         menus: [
           {
-            title : '에듀싸피',
+            title : 'Edu',
+            imag:'https://edu.ssafy.com/asset/images/logo.png',
             urls:'https://edu.ssafy.com/'
           },
           {
-            title : '지라',
+            title : 'Project',
+            imag:'https://edu.ssafy.com/asset/images/logo.png',
+            urls:'https://project.ssafy.com/'
+          },
+          {
+            title : 'Jira',
+            imag:'https://luna1.co/5ad265.png',
             urls:'https://jira.ssafy.com/'
           },
           {
-            title : '깃랩',
+            title : 'Lab',
+            imag:'https://assets.gitlab-static.net/uploads/-/system/group/avatar/9970/logo-extra-whitespace.png',
             urls:'https://lab.ssafy.com/'
           },
-          {
-            title : '프로젝트',
-            urls:'https://project.ssafy.com/'
-          },
+          
         ],
         open: false,
-        placeholder: "SiteMap",
       }
     }
   },
@@ -69,7 +77,11 @@ export default {
     },
     toggle() {
       console.log(this.open)
-      this.config.open = !this.config.open
+      this.config.open = !this.config.open;
+    },
+    openModal() {
+      this.config.open = false;
+      this.showModal = true;
     },
     closeModal() {
             console.log('clicked');
@@ -85,46 +97,72 @@ export default {
 
 
 <style lang="scss" scoped>
-// dropdown test
+// dropdown
 .dropdowns {
-  position: relative;
-  width: 100px;
-  height: 30px;
-  background-color: var(--color-primary);
-  z-index: 1;
+  width: 150px;
+  height: 50px;
+  background-color: var(--color-surface);
+  border-radius: calc(var(--radius, 0.25em)/2);
+  padding: var(--space-sm);
+  border: 1px solid var(--color-surface-dark);
+  
+}
+
+.dropdowns:hover {
+  box-shadow: var(--shadow-sm);
 }
 
 .open .dropdowns-menu {
-  display: block;
+  visibility: visible;
+  opacity: 1;
 }
 .dropdowns-menu {
   position: relative;
+  display: flex;
+  justify-content: start;
   text-align: center;
-  width: 100px;
-  top:-105px;
-  display: none;
-  background-color: var(--color-secondary);
-}
-
-.dropdowns-menu li:hover {
+  width: 150px;
+  height: 50px;
+  right: 12px;
+  top: -233px;
+  padding: var(--space-sm);
+  visibility: hidden;
+  opacity: 0;
+  transition: visibility 0s, opacity .2s linear;
+  border: 1px solid var(--color-surface-dark);
   background-color: var(--color-surface);
 }
 
+.dropdowns-menu a li {
+  color: var(--color-on-surface);
+  font-weight: 500;
+}
 
+
+.dropdowns-menu:hover {
+  cursor: pointer;
+  background-color: var(--color-surface-darker);
+}
+
+//contact
+.contact:hover {
+  cursor: pointer;
+  color: var(--color-contrast-high);
+}
 
 // colophon
 .main-footer__colophon {
     border-top: 1px solid var(--color-contrast-low);
-    padding-top: var(--space-xxxs);
     margin-top: var(--space-md);
     display: flex;
     justify-content: space-between;
     flex-wrap: wrap;
     align-items: center;
+    background-color: var(--color-contrast-darker);
 }
 
 .main-footer__colophon-nav {
-    font-size: var(--text-sm);
+    font-size: var(--text-lg);
     color: var(--color-contrast-medium);
     margin-right: var(--space-sm);
 
