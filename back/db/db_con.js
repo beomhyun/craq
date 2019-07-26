7df8fa3a079a86638a9c42a6f10d364adc0350c8
beomhyunkim <beomhyunkim@gmail.com>
2019-07-26 15:06:37 +0900

    기존 프로젝트에서 사용했던 로그 남기기위한 함수 삭제

--- back/db/db_con.js
+++ back/db/db_con.js
@@ -1,23 +1,6 @@
 var mysql = require('mysql');
 var config = require('./db_info').local;
-var log = function(){
-  // Inner logic
-  function write_log(con,user_id,query){
-    var sql = " insert into weblog(user_id,log,time) values( ? , ? ,now()) "
-    var params = [user_id, query];
-    con.query(sql, params, function(err, rows, fields) {
-      if (!err){
-      }
-      else{
-        console.log('Error while performing Query.', err);
-      }
-    })
-  }
-  // Expose API
-  return {
-    write_log: write_log
-  }
-}()
+
 var  Connection;
 module.exports = function () {
   return {
