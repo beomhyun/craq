49692d32653be8f1c789c610a165cdd6e93d3eac
beomhyunkim <beomhyunkim@gmail.com>
2019-07-29 15:44:31 +0900

    user_token 헤더로 변경, 토픽 검색 기능 추가

--- back/model/topic.js
+++ back/model/topic.js
@@ -69,12 +69,12 @@ const initializeEndpoints = (app) => {
 
   /**
    * @swagger
-   *  /topics/{user_token}:
+   *  /topics:
    *    get:
    *      tags:
    *      - topic
    *      parameters:
-   *       - in: path
+   *       - in: header
    *         name: user_token
    *         type: string
    *         description: |
@@ -83,8 +83,8 @@ const initializeEndpoints = (app) => {
    *      responses:
    *        200:
    */
-  app.get('/topics/:user_token', function(req, res) {
-    jwt.verify(req.params.user_token, secretObj.secret, function(err, decoded) {
+  app.get('/topics', function(req, res) {
+    jwt.verify(req.headers.user_token, secretObj.secret, function(err, decoded) {
       if (err) res.status(401).send({
         error: 'invalid token'
       });
@@ -103,12 +103,12 @@ const initializeEndpoints = (app) => {
 
   /**
    * @swagger
-   *  /topics/actived/{user_token}:
+   *  /topics/actived:
    *    get:
    *      tags:
    *      - topic
    *      parameters:
-   *       - in: path
+   *       - in: header
    *         name: user_token
    *         type: string
    *         description: |
@@ -117,8 +117,8 @@ const initializeEndpoints = (app) => {
    *      responses:
    *        200:
    */
-  app.get('/topics/actived/:user_token', function(req, res) {
-    jwt.verify(req.params.user_token, secretObj.secret, function(err, decoded) {
+  app.get('/topics/actived', function(req, res) {
+    jwt.verify(req.headers.user_token, secretObj.secret, function(err, decoded) {
       if (err) res.status(401).send({
         error: 'invalid token'
       });
@@ -137,7 +137,7 @@ const initializeEndpoints = (app) => {
 
   /**
    * @swagger
-   *  /topics/{id}/{user_token}:
+   *  /topics/{id}:
    *    put:
    *      tags:
    *      - topic
@@ -147,7 +147,7 @@ const initializeEndpoints = (app) => {
    *        in: body
    *        schema:
    *          $ref: '#/definitions/topicInfo'
-   *      - in: path
+   *      - in: header
    *        name: user_token
    *        type: string
    *        description: |
@@ -155,8 +155,8 @@ const initializeEndpoints = (app) => {
    *      responses:
    *        200:
    */
-  app.put('/topics/:id/:user_token', function(req, res) {
-    jwt.verify(req.params.user_token, secretObj.secret, function(err, decoded) {
+  app.put('/topics/:id', function(req, res) {
+    jwt.verify(req.headers.user_token, secretObj.secret, function(err, decoded) {
       if (err) res.status(401).send({
         error: 'invalid token'
       });
@@ -177,7 +177,7 @@ const initializeEndpoints = (app) => {
 
   /**
    * @swagger
-   *  /topics/{id}/{user_token}:
+   *  /topics/{id}:
    *    delete:
    *      tags:
    *      - topic
@@ -186,7 +186,7 @@ const initializeEndpoints = (app) => {
    *      - name: id
    *        in: path
    *        type: integer
-   *      - in: path
+   *      - in: header
    *        name: user_token
    *        type: string
    *        description: |
@@ -194,8 +194,8 @@ const initializeEndpoints = (app) => {
    *      responses:
    *        200:
    */
-  app.delete('/topics/:id/:user_token', function(req, res) {
-    jwt.verify(req.params.user_token, secretObj.secret, function(err, decoded) {
+  app.delete('/topics/:id', function(req, res) {
+    jwt.verify(req.headers.user_token, secretObj.secret, function(err, decoded) {
       if (err) res.status(401).send({
         error: 'invalid token'
       });
@@ -212,6 +212,50 @@ const initializeEndpoints = (app) => {
       }
     });
   });
-};
+ /**
+   * @swagger
+   *  /topics/search:
+   *    get:
+   *      tags:
+   *      - topic
+   *      parameters:
+   *       - in: query
+   *         name: title
+   *         type: string
+   *         description: |
+   *          검색할 게시판 제목 전달
+   *       - in: header
+   *         name: user_token
+   *         type: string
+   *         description: |
+   *          사용자 토큰 전달
+   *      description: 사용자의 토큰, 제목 검색 키워드를 받아 활성화된 게시판을 보여줌.
+   *      responses:
+   *        200:
+   */
+  app.get('/topics/search', function(req, res) {
+    console.log(req.headers.user_token);
+    jwt.verify(req.headers.user_token, secretObj.secret, function(err, decoded) {
+      if (err) res.status(401).send({
+        error: 'invalid token'
+      });
+      else {
+        //var sql = ' SELECT pk,topic FROM topic WHERE topic like "'+'%' + req.query.title + '%" and is_active =  1 ';
+        sql = ` SELECT pk,topic FROM topic WHERE topic like "%${req.query.title}%" and is_active =  ${TRUE}`;
+        connection.query(sql, function(err, rows, fields) {
+          if (!err) {
+            console.log(this.sql);
+            res.json(rows);
+          } else {
+            console.log(err);
+            res.send(err);
+          }
+        });
+      }
+    });
+  });
 
+
+};
+ 
 module.exports = initializeEndpoints;
