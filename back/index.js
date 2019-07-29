c4ec68951756ffa9e604329f4cb5766e8a8de197
KGH <rngus3050@gmail.com>
2019-07-29 16:52:32 +0900

    이제 보내주시나요

--- back/index.js
+++ back/index.js
@@ -1,14 +1,23 @@
 const express = require('express');
-const endpoints = require('./model/user');
+const user = require('./model/user');
 const topic = require('./model/topic');
+const notice = require('./model/notice');
 const swaggerDoc = require('./swaggerDoc');
 const bodyParser = require('body-parser');
 const app = express(express);
 
+app.use(express.static('upload'));
 app.use(bodyParser.json());
-endpoints(app);
+user(app);
 topic(app);
+notice(app);
 swaggerDoc(app);
+
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
 
 app.use((err,req,res,next) => console.error('There was an error',err));
 
