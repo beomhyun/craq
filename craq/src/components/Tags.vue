99c04ac550795a80cf10f52ecadc23938efe5857
jchrys <lovedr0p@naver.com>
2019-07-30 17:17:42 +0900

    Tags

---
+++ craq/src/components/Tags.vue
@@ -0,0 +1,143 @@
+<template>
+    <div>
+        <div class="filler">
+
+            <!-- Headline -->
+            <div class="headline">
+                {{currentRouteName}}
+            </div> <!-- headline -->
+
+            <!-- Filter && Card -->
+            <div class="subnav">
+                <div class="subnav__filter">
+                    <div class="btn btn--sm">Latest</div>
+                    <div class="btn btn--sm">Reliable</div>
+                    <div class="btn btn--sm">Helpful</div>
+                    <div class="btn btn--sm">Answer</div>
+                    <div class="btn btn--sm">View</div>
+                </div>
+            </div> <!-- Filter /div -->
+
+            <div class="grid  grid-gap-xl margin-lg">
+                <template v-for="tag in tags" v-key="i">
+                    <TagsCard class="col-3" :title="tag.title" :body="tag.body" :id="tag.id"></TagsCard>
+                </template>
+            </div>
+
+
+            <!-- Pagenation -->
+            <div class="pagenation">
+                <ul v-for="(i, index) in (1,11)">
+                    <li>{{index}}</li>
+                </ul>
+            </div>  <!-- pagenation -->
+
+        </div> <!-- Container /div -->
+
+    </div>  <!-- Filler -->
+</template>
+
+<script>
+//import Card from '@/components/Card.vue';
+//import Ask from '@/components/AskQuestion.vue';
+import TagsCard from '@/components/TagsCard.vue';
+import axios from 'axios';
+const apiUrl = "https://jsonplaceholder.typicode.com/posts"
+export default {
+    name: "Tags",
+    components: {
+        //Card,
+        //Ask,
+        TagsCard
+    },
+    data() {
+        return {
+            messages: '',
+            //latested: true,
+            //cardLists: [],
+            tags: [],
+        }
+    },
+    computed: {
+        currentRouteName() {
+            console.log(this.$route.name);
+            return this.$route.name;
+        },
+    },
+    mounted () {
+        axios.get(apiUrl)
+            .then(res=> {
+                this.tags = res.data;
+            })
+        
+    }
+}
+
+</script>
+
+<style scoped lang="scss">
+// Test End
+.filler {
+    height: 1000px;
+    width: auto;
+    background-color: var(--color-background);
+}
+
+.headline {
+    background-color: var(--color-background);
+    padding: var(--space-xs);
+    width: 100%;
+    height: 75px;
+    font-size: var(--text-xxl);
+    text-transform: capitalize;
+}
+
+.pagenation {
+    display: flex;
+    justify-content: center;
+    background-color: var(--color-background);
+    margin-top: var(--space-lg);
+    width: 100%;
+    height: 30px;
+    font-size: var(--text-lg)
+}
+
+.pagenation ul li{
+    display: inline;
+}
+
+.subnav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: var(--color-background);
+    border-radius: var(--radius-sm);
+    width: 100%;
+    font-size: 20px;
+    padding-left: 20px;
+    padding-right: 20px;
+
+    &__filter {
+        background-color: var(--color-surface);
+        color: var(--color-on-surface);
+    }
+    &__filter-seleted {
+        background-color: var(--color-primary);
+        color: var(--color-on-primary)
+    }
+}
+
+.btn {
+    margin-right: var(--space-xxs); 
+}
+
+.btn:hover {
+    background-color: var(--color-primary-light);
+    color: var(--color-on-primary-light);
+}
+
+.shadow:hover {
+    cursor: pointer;
+}
+
+</style>
---
+++ craq/src/components/TagsCard.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="p-table">
+        <div class="p-table__item p-table__item--popular col-4@md">
+            <div class="flex justify-between items-center margin-bottom-xxs">
+                <h4 class="p-table__title"><v-clamp autoresize :max-lines="1">{{title}}</v-clamp></h4>
+                <span class="p-table__badge">{{id}}</span>
+            </div>
+
+            <div class="p-table__count margin-bottom-sm"><span>#151</span> <i>/ view1</i></div>
+
+            <ul class="p-table__features margin-bottom-md">
+                <li>css : 1000</li>
+                <li>Html5: 500</li>
+                <li>Cpp: 10000</li>
+                <li>Java: 50</li>
+                <li>VueJs: 50000</li>
+            </ul>
+
+            <div class="margin-top-auto"><a href="#0" class="btn btn--primary btn--md width-100%">redirect</a></div>
+        </div>
+    </div>
+</template> 
+
+<script>
+import VClamp from 'vue-clamp'
+export default {
+    name: "TagsCard",
+    props: [
+        "title", "body", "id"
+    ],
+    components: {
+        VClamp
+    }
+}
+
+</script>
+
+
+<style lang="scss" scoped>
+.p-table {
+    &__item {
+        background-color: var(--color-background);
+        border-radius: var(--radius-md);
+        padding: var(--space-md);
+        display: flex;
+        flex-direction: column;
+
+        &--popular {
+            background-color: var(--color-surface);
+            color: var(--color-on-surface);
+            border: 2px solid var(--color-primary-dark);
+        }
+
+    }
+
+    &__badge {
+        font-size: var(--text-sm);
+        background-color: var(--color-contrast-high);
+        color: var(--color-bg);
+        border-radius: var(--radius-sm);
+    }
+
+    &__count {
+        span {
+            font-size: var(--text-xxxl);
+            font-weight: bold;
+        }
+    }
+
+    &__features {
+        li {
+            margin-bottom: var(--space-xs);
+        }
+    }
+
+}
+.btn--primary {
+    background-color: var(--color-primary-dark);
+}
+</style>
