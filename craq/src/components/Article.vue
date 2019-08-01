c6f67661f034397e761477fa047183c9acd95906
jchrys <lovedr0p@naver.com>
2019-08-01 17:04:34 +0900

    :hammer: CodeDetail

---
+++ craq/src/components/Article.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="post-layout">
+        <div class="votecell post-layout--left">
+            <CodeDetailVote></CodeDetailVote>
+        </div>
+        <div class="postcell post-layout--right">
+            <CodeDetailContent></CodeDetailContent>
+            <CodeDetailTagList></CodeDetailTagList>
+            <CodeDetailQuestionSignature></CodeDetailQuestionSignature>
+            <div style="grid-column: 1 / 3;"></div>
+            <div class="post-layout--right">
+                <CodeDetailComments></CodeDetailComments>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import CodeDetailVote from '@/components/CodeDetailVote.vue';
+import CodeDetailTagList from '@/components/CodeDetailTagList.vue';
+import CodeDetailQuestionSignature from '@/components/CodeDetailQuestionSignature';
+import CodeDetailComments from '@/components/CodeDetailComments.vue';
+import CodeDetailContent from '@/components/CodeDetailContent.vue';
+
+export default {
+    name: "Article",
+    components: {
+        CodeDetailContent,
+        CodeDetailComments,
+        CodeDetailVote,
+        CodeDetailTagList,
+        CodeDetailQuestionSignature,
+    }
+}
+
+</script>
+
+<style lang="scss" scoped>
+.votecell {
+    vertical-align: top;
+    padding-right: 15px;
+    width: auto;
+}
+.postcell {
+    vertical-align: top;
+}
+
+.post-layout {
+    display: grid;
+    grid-template-columns: max-content 1fr;
+
+    &--left {
+        width: auto;
+        padding-right: 15px;
+    }
+    &--right { //TODO
+        grid-column: 2;
+        width: auto;
+        flex-shrink: 1;
+    }
+}
+
+</style>
--- craq/src/components/CodeDetail.vue
+++ craq/src/components/CodeDetail.vue
@@ -7,20 +7,7 @@
                 <div id="mainbar">
 
                     <div id="question" class="question">
-                        <div class="post-layout">
-                            <div class="votecell post-layout--left">
-                                <CodeDetailVote></CodeDetailVote>
-                            </div>
-                            <div class="postcell post-layout--right">
-                                <CodeDetailContent></CodeDetailContent>
-                                <CodeDetailTagList></CodeDetailTagList>
-                                <CodeDetailQuestionSignature></CodeDetailQuestionSignature>
-                                <div style="grid-column: 1 / 3;"></div>
-                                <div class="post-layout--right">
-                                    <CodeDetailComments></CodeDetailComments>
-                                </div>
-                            </div>
-                        </div>
+                        <Article></Article>
                     </div>
 
                 </div>
@@ -33,22 +20,14 @@
 <script>
 import CodeDetailQuestionHeader from '@/components/CodeDetailQuestionHeader.vue';
 import CodeDetailQuestionHeaderBottom from '@/components/CodeDetailQuestionHeaderBottom.vue';
-import CodeDetailVote from '@/components/CodeDetailVote.vue';
-import CodeDetailTagList from '@/components/CodeDetailTagList.vue';
-import CodeDetailQuestionSignature from '@/components/CodeDetailQuestionSignature';
-import CodeDetailComments from '@/components/CodeDetailComments.vue';
-import CodeDetailContent from '@/components/CodeDetailContent.vue';
+import Article from '@/components/Article.vue';
 
 export default {
     name: "CodeDetail",
     components: {
-        CodeDetailContent,
-        CodeDetailComments,
         CodeDetailQuestionHeader,
         CodeDetailQuestionHeaderBottom,
-        CodeDetailVote,
-        CodeDetailTagList,
-        CodeDetailQuestionSignature,
+        Article,
     }
 }
 
@@ -67,29 +46,6 @@ export default {
 }
 
 
-.votecell {
-    vertical-align: top;
-    padding-right: 15px;
-    width: auto;
-}
-.postcell {
-    vertical-align: top;
-}
-
-.post-layout {
-    display: grid;
-    grid-template-columns: max-content 1fr;
-
-    &--left {
-        width: auto;
-        padding-right: 15px;
-    }
-    &--right { //TODO
-        grid-column: 2;
-        width: auto;
-        flex-shrink: 1;
-    }
-}
 
 .clearfix::after {
     content: "";
