4cde6b56be7846c0bdf4d7b41d9e7f5d180e5f6d
josujang <whtnwkd@naver.com>
2019-08-05 17:36:48 +0900

    1. get wards/:article_id/:user_id api추가   - ward의 기록 여부를 알려줌(success: 있음, fail : 없음)

--- back/model/ward.js
+++ back/model/ward.js
@@ -36,14 +36,13 @@ const initializeEndpoints = (app) => {
     jwt.verify(req.body.user_token, secretObj.secret, function(err, decoded) {
       if (err) {
         res.status(401).send({
-        error: 'invalid token'
-      });
-      serverlog.log(connection,decoded.pk,this.sql,"fail",req.connection.remoteAddress);
-    }
-      else {
+          error: 'invalid token'
+        });
+        serverlog.log(connection, decoded.pk, this.sql, "fail", req.connection.remoteAddress);
+      } else {
         // 와드 기록이 있는지 없는지 확인 (=COUNT)
         var sql =
-        `
+          `
           SELECT  COUNT(ARTICLE) AS COUNT
           FROM    WARD
           WHERE   ARTICLE = ${req.body.article_id}
@@ -51,17 +50,17 @@ const initializeEndpoints = (app) => {
         `;
         connection.query(sql, function(err, rows, fields) {
           if (!err) {
-            if(rows[0].COUNT == 0){
+            if (rows[0].COUNT == 0) {
               sql =
-              `
+                `
                 INSERT  INTO
                 WARD    ( ARTICLE, USER )
                 VALUES  ( ${req.body.article_id}, ${req.body.user_id})
               `;
-            }else{
+            } else {
               // 이전 기록이 있을 때는 is_remove만 반대처리해준다
               sql =
-              `
+                `
                 UPDATE  WARD
                 SET     IS_REMOVED = ABS(IS_REMOVED-1)
                 WHERE   ARTICLE    = ${req.body.article_id}
@@ -69,17 +68,73 @@ const initializeEndpoints = (app) => {
               `;
             }
             connection.query(sql, function(err, rows, fields) {
-              if (!err){
-                serverlog.log(connection,decoded.pk,this.sql,"success",req.connection.remoteAddress);
-                res.send({status: "success"});
-              }else{
-                serverlog.log(connection,decoded.pk,this.sql,"fail",req.connection.remoteAddress);
-                res.send({status: "fail"});
+              if (!err) {
+                serverlog.log(connection, decoded.pk, this.sql, "success", req.connection.remoteAddress);
+                res.send({
+                  status: "success"
+                });
+              } else {
+                serverlog.log(connection, decoded.pk, this.sql, "fail", req.connection.remoteAddress);
+                res.send({
+                  status: "fail"
+                });
               }
             });
           } else {
-            serverlog.log(connection,decoded.pk,this.sql,"fail",req.connection.remoteAddress);
-            res.send({status: "fail"});
+            serverlog.log(connection, decoded.pk, this.sql, "fail", req.connection.remoteAddress);
+            res.send({
+              status: "fail"
+            });
+          }
+        });
+      }
+    });
+  });
+
+  /**
+   * @swagger
+   *  /wards/{article_id}/{user_id}:
+   *    get:
+   *      tags:
+   *      - ward
+   *      description: 게시글의 와드 여부를 가져옴 success -> 와드 기록 있음,   fail-> 와드 기록 없음
+   *      parameters:
+   *      - name: article_id
+   *        in: path
+   *        type: integer
+   *        description: article id 값
+   *      - name: user_id
+   *        in: path
+   *        type: integer
+   *        description: user의 id값
+   *      - name: user_token
+   *        in: header
+   *        type: string
+   *        description: 사용자의 token값을 전달.
+   *      responses:
+   *        200:
+   */
+  app.get('/wards/:article_id/:user_id', function(req, res) {
+    jwt.verify(req.headers.user_token, secretObj.secret, function(err, decoded) {
+      if (err) {
+        res.status(401).send({
+          error: 'invalid token'
+        });
+      } else {
+        var sql =
+        `
+        SELECT  COUNT(ARTICLE) AS COUNT
+        FROM    WARD
+        WHERE   ARTICLE = ${req.params.article_id}
+        AND     USER    = ${req.params.user_id}
+        `;
+        connection.query(sql, function(err, rows, fields) {
+          if (!err && rows[0].COUNT == 1) {
+              serverlog.log(connection, decoded.pk, this.sql, "success", req.connection.remoteAddress);
+              res.send({ status: "success" });
+          } else {
+              serverlog.log(connection, decoded.pk, this.sql, "fail", req.connection.remoteAddress);
+              res.send({ status: "fail" });
           }
         });
       }
