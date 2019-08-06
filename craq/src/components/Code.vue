c9ec4ce16e921071f960334eb9b60186ddf90565
KGH <rngus3050@gmail.com>
2019-08-06 17:28:33 +0900

    중간정검

--- craq/src/components/Code.vue
+++ craq/src/components/Code.vue
@@ -20,7 +20,7 @@
 
             <div class="Code__list" v-show="setLatest">
               <div :key="idx" v-for="(list, idx) in askData" class="content">
-                <router-link :to="{ name: 'Questions', params: { pk : list.PK}}">
+                <router-link :to="{ name: 'Questions', params: { question_pk : list.PK}}">
                   <card class="shadow" :list="list"/>
                 </router-link>
               </div>
