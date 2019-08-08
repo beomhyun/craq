79f7fa15c4d381bfda1f7a627a42c9ed4ce69c48
josujang <whtnwkd@naver.com>
2019-08-08 15:46:15 +0900

    comment,content post할 때에 따른 insert notice 정보를 info에 넣어 전송함

--- back/model/content.js
+++ back/model/content.js
@@ -68,8 +68,8 @@ const initializeEndpoints = (app) => {
    *    post:
    *      tags:
    *      - content
-   *      description: content를 작성, 질문&작성글일 땐 topic = 1 이며 , 질문글을 작성할 때는 article_id = 0 , 답변글을 작성할 때
-   *                   답변을 달을 질문 article id값을 입력
+   *      description: content를 작성, 질문&작성글일 땐 topic = 1 이며 , 질문글을 작성할 때는 article_id = 0
+   *                   최초 작성일 떄는 beforeContent = 0
    *      parameters:
    *      - in: body
    *        name: contentsbody
@@ -110,7 +110,7 @@ const initializeEndpoints = (app) => {
       } else {
         if (i.beforeContent == 0) { // 이전에 작성한 content가 없는 최초의 article 작성일 때
           sql =
-            `
+          `
           INSERT    INTO
           CONTENT   (TITLE,BODY,IMAGE,CREATEDUSER,UPDATEDUSER)
           VALUES    ('${i.title}','${i.body}','${i.image}',${i.user_id},${i.user_id})
@@ -128,7 +128,7 @@ const initializeEndpoints = (app) => {
                 if (!err) {
                   var articleId = rows.insertId; // 방금 생성된 article의 id값
                   sql = // 방금 생성된 article의 id값으로 갱신시켜준다.
-                    `
+                  `
                   UPDATE  CONTENT
                   SET     ARTICLE = ${articleId}
                   WHERE   PK      = ${contentId}
@@ -164,54 +164,63 @@ const initializeEndpoints = (app) => {
                       }
                       toArray(i.tags, decoded.pk, contentId);
 
-
                       if (i.topic_id == 1 && i.article_id != 0) { // 작성한 글이 어느 질문에 대한 답변일 때
                         sql =
-                          `
-                          SELECT    A.CREATEDUSER
-                                  , C.TITLE
-                          FROM      ARTICLE AS A
-                          JOIN      CONTENT AS C
-                          ON        A.CONTENT = C.PK
-                          WHERE     A.PK = ${i.article_id}
-                        `;
-
+                        `
+                        SELECT	A.ARTICLE AS QUESTION_PK
+                          		 , (
+                            		 	SELECT 	CONTENT
+                            		 	FROM 		ARTICLE
+                            		 	WHERE 	PK = A.ARTICLE
+                          		 ) QUESTION_CONTENT
+                          		 , A.PK 		 AS ANSWER_PK
+                          		 , C.PK 		 AS ANSWER_CONTENT
+                          		 , C.TITLE
+                          		 , A.CREATEDUSER
+                          		 , (
+                            		 	SELECT	USERNAME
+                            		 	FROM 		USER
+                            		 	WHERE 	PK = A.CREATEDUSER
+                          		 ) USERNAME
+                        FROM 		ARTICLE A
+                        JOIN 		CONTENT C
+                        ON 		  A.CONTENT    = C.PK
+                        WHERE   A.PK         = ${i.article_id}
+                        `;  // 질문글의 작성자를 얻어옴
                         connection.query(sql, function(err, rows, fields) {
-                          var created_user = rows[0].CREATEDUSER;
-                          var title = rows[0].TITLE;
-                          var msg = `${title} 글에 답변이 달렸습니다`; // 질문 작성자에게 답변이 달렸음을 알려주는 알림정보를 추가한다.
-
+                          var msg = `${rows[0].TITLE} 글에 답변이 달렸습니다`; // 질문 작성자에게 답변이 달렸음을 알려주는 알림정보를 추가한다.
+                          var qst_pk = rows[0].QUESTION_PK;
+                          var qst_content = rows[0].QUESTION_CONTENT;
+                          var ans_pk = rows[0].ANSWER_PK;
+                          var ans_content = rows[0].ANSWER_CONTENT;
+                          var info = `{question_pk:${qst_pk}, question_content:${qst_content}, answer_pk:${ans_pk}, answer_content:${ans_content}}`;
                           sql =
                           `
                           INSERT  INTO
-                          NOTICE  (USER,TYPE,BODY,ARTICLE,CONTENT)
-                          VALUES  (${created_user},1,'${msg}',${i.article_id},${contentId})
+                          NOTICE  (USER,TYPE,BODY,INFO)
+                          VALUES  (${rows[0].CREATEDUSER},1,'${msg}','${info}')
                           `;
                           connection.query(sql, function(err, rows, fields) {
                             if (!err) {
                               serverlog.log(connection, decoded.pk, this.sql, "success", req.connection.remoteAddress);
-                              //res.send({status: "success2"});
                             } else {
                               serverlog.log(connection, decoded.pk, this.sql, "fail", req.connection.remoteAddress);
-                              //res.send({status: "fail4"});
                             }
                           });
-
-                          sql = // 답변을 작성한 직후에 답변 article의 article 컬럼을 찾는다.
+                          sql = // 답변을 작성한 직후에 와드를 설정한 사용자들의 수를 구한다.
                           `
                           SELECT  COUNT(*) COUNT
                           FROM    WARD
-                          WHERE   ARTICLE = ${i.article_id}
+                          WHERE   ARTICLE    = ${i.article_id}
                           AND     IS_REMOVED = 0
                           `;
                           connection.query(sql, function(err, rows, fields) {
-                            console.log("sum="+rows[0].COUNT);
                             if (!err && rows[0].COUNT > 0) {
                               sql =
                               `
                               SELECT  USER
                               FROM    WARD
-                              WHERE   ARTICLE = ${i.article_id}
+                              WHERE   ARTICLE    = ${i.article_id}
                               AND     IS_REMOVED = 0
                               `;
                               connection.query(sql, function(err, rows, fields) {
@@ -220,8 +229,8 @@ const initializeEndpoints = (app) => {
                                     sql =
                                     `
                                     INSERT  INTO
-                                    NOTICE  (USER,TYPE,BODY,ARTICLE,CONTENT)
-                                    VALUES  (${rows[i].USER},1,'${msg}',${i.article_id}, ${contentId})
+                                    NOTICE  (USER,TYPE,BODY,INFO)
+                                    VALUES  (${rows[i].USER},1,'${msg}','${info}')
                                     `;
                                     connection.query(sql, function(err, rows, fields) {
                                       if (!err) {
@@ -234,7 +243,7 @@ const initializeEndpoints = (app) => {
                                   serverlog.log(connection, decoded.pk, this.sql, "success", req.connection.remoteAddress);
                                   res.send({
                                     status: "success",
-                                    msg: "insert notice"
+                                    msg: "insert notice2",
                                   });
                                 } else {
                                   serverlog.log(connection, decoded.pk, this.sql, "fail", req.connection.remoteAddress);
