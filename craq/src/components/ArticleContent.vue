889dd63111870b9e0b418c679daf0fa948c6a3be
jchrys <lovedr0p@naver.com>
2019-08-08 14:00:12 +0900

    :hotfix:

--- craq/src/components/ArticleContent.vue
+++ craq/src/components/ArticleContent.vue
@@ -5,8 +5,12 @@
 </template>
 
 <script>
+import VClamp from 'vue-clamp'
 export default {
     name: "ArticleContent",
+    components: {
+        VClamp
+    },
     props: [
         "body"
     ]
@@ -21,6 +25,7 @@ export default {
     font-size: 1rem; // TODO
     line-height: 1.3;
     font-family: Arial, "Helvetica Neue",Helvetica, sans-serif;
+    width: 1100px;
 }
 
 
