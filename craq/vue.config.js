cce76bbb1638e8f0740cc462c7cdb1f54565b3f1
김구현 <siva3020@naver.com>
2019-08-23 15:17:02 +0900

    Host CHeck 설정 변경

--- craq/vue.config.js
+++ craq/vue.config.js
@@ -1,6 +1,11 @@
 const path = require("path");
 
 module.exports = {
+  devServer: {
+      host : 'localhost',
+      port : 8080, 
+      disableHostCheck : true
+    },
   transpileDependencies: ['vue-clamp', 'resize-detector'],
   pluginOptions: {
     "style-resources-loader": {
@@ -9,10 +14,6 @@ module.exports = {
     }
   },
   chainWebpack: (config) => {
-    config.devServer
-      .host('15.164.153.221')
-      .port(8080)
-      .disableHostCheck(true)
     config.plugins.delete('preload')
   }
 };
